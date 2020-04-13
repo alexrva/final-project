aba1e7098fbe5e82c1f36dec15352765e029dd6a
Greg Smith <thinkable@thinkable.us>
2020-04-12 20:34:17 -0400

    integrating Dinesh's table

--- final/static/js/citypage.js
+++ final/static/js/citypage.js
@@ -1,17 +1,4 @@
 //from search URL paramter, determine what city is being called
-function queryStringParser() {
-  let results = {};
-  let words = window.location.search.substring(1).split('&');
-  for (word of words) {
-    let [key, value] = word.split("=");
-    results[key] = [value];
-  }
-  return results;
-}
-
-let parms = queryStringParser();
-let city = decodeURIComponent(parms.city);
-console.log(city);
 
 let cityrank = "";
 let state = "";
